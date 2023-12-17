11987cd0a7172d9a3e3600b4dd131a87d92fbc67
Lim Chae Won <wonitch03@gmail.com>
2023-12-17 12:11:28 +0900

    Organized Code & Testing 2-Board Config with CAN Transiver

---
+++ Code/MainBoard_CAN/MainBoard_CAN.ino
@@ -0,0 +1,37 @@
+#include <CAN.h>
+#include 
+
+using namespace Sensor;
+class IAT
+{
+private:
+    /* data */
+public:
+    IAT(/* args */);
+    ~IAT();
+};
+
+IAT::IAT(/* args */)
+{
+}
+
+IAT::~IAT()
+{
+}
+
+class TPS
+{
+private:
+    int ThrAngle;
+public:
+    TPSread(/* args */);
+    ~TPS();
+};
+
+TPS::TPSread(/* args */)
+{
+}
+
+TPS::~TPS()
+{
+}
