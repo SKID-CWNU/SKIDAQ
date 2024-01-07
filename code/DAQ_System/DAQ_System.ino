9b7ec248d0fb54c7c4e59ab7b627fccba1891d86
Lim Chae Won <wonitch03@gmail.com>
2024-01-07 13:12:15 +0900

    1st Attempt to init CAN Interface

--- code/DAQ_System/DAQ_System.ino
+++ code/DAQ_System/DAQ_System.ino
@@ -4,13 +4,8 @@
 
 #include <Arduino.h>
 #include <SPI.h>
+#include <CAN.h>
 #include <DHT.h>
-
-const int SPI_CS_PIN = 22;
-const int CAN_INT_PIN = 20;
-
-#include "mcp2515_can.h"
-mcp2515_can CAN(SPI_CS_PIN); // Set CS pin
 
 #define DHTPIN 21
 #define DHTTYPE DHT22
@@ -18,18 +13,18 @@ DHT dht(DHTPIN, DHTTYPE);
 
 void setup()
 {
-   
+
     Serial.begin(115200);
     while (!Serial)
     {
     };
-
-    while (CAN_OK != CAN.begin(CAN_500KBPS))
-    { // init can bus : baudrate = 500k
-        Serial.println("CAN init fail, retry...");
-        delay(100);
+    // start the CAN bus at 500 kbps
+    if (!CAN.begin(500E3))
+    {
+        Serial.println("Starting CAN failed!");
+        while (1)
+            ;
     }
-    Serial.println("CAN init ok!");
     dht.begin();
 }
 
@@ -44,22 +39,17 @@ void loop()
         Serial.println(F("Failed to read from DHT sensor!"));
         return;
     }
-    unsigned char stmp[8] = {char(h), char(t), 0, 0, 0, 0, 0, 0};
+    Serial.print("Sending DHT packet ... ");
 
-    // send data:  id = 0x00, standrad frame, data len = 8, stmp: data buf
-    stmp[7] = stmp[7] + 1;
-    if (stmp[7] == 100)
-    {
-        stmp[7] = 0;
-        stmp[6] = stmp[6] + 1;
+    CAN.beginPacket(0x12);
+    CAN.write("d");
+    CAN.write(h);
+    CAN.write(t);
+    CAN.write();
+    CAN.write();
+    CAN.endPacket();
 
-        if (stmp[6] == 100)
-        {
-            stmp[6] = 0;
-            stmp[5] = stmp[5] + 1;
-        }
-    }
+    Serial.println("done");
 
-    CAN.sendMsgBuf(0x00, 0, 8, stmp);
     delay(100); // send data per 100ms
 }
--- code/MainBoard_CAN/MainBoard_CAN.ino
+++ code/MainBoard_CAN/MainBoard_CAN.ino
@@ -2,53 +2,10 @@
 // Solenoid Valve Controlled QuickShift Interface //
 // Author: Lim Chae Won //
 
+#include <Arduino.h>
 #include <SPI.h>
-#include <can-serial.h>
-#include <mcp_can.h>
-#include <mcp2515_can_dfs.h>
-#include <mcp2515_can.h>
-#include <mcp2518fd_can_dfs.h>
-#include <mcp2518fd_can.h>
+#include <CAN.h>
 #include <SoftwareSerial.h>
-
-#define CAN_2515
-// #define CAN_2518FD
-
-// Set SPI CS Pin according to your hardware
-
-#if defined(SEEED_WIO_TERMINAL) && defined(CAN_2518FD)
-// For Wio Terminal w/ MCP2518FD RPi Hat：
-// Channel 0 SPI_CS Pin: BCM 8
-// Channel 1 SPI_CS Pin: BCM 7
-// Interupt Pin: BCM25
-const int SPI_CS_PIN = BCM8;
-const int CAN_INT_PIN = BCM25;
-#else
-
-// For Arduino MCP2515 Hat:
-// the cs pin of the version after v1.1 is default to D9
-// v0.9b and v1.0 is default D10
-const int SPI_CS_PIN = 22;
-const int CAN_INT_PIN = 20;
-#endif
-
-#ifdef CAN_2518FD
-#include "mcp2518fd_can.h"
-mcp2518fd CAN(SPI_CS_PIN); // Set CS pin
-
-// To TEST MCP2518FD CAN2.0 data transfer
-#define MAX_DATA_SIZE 8
-// To TEST MCP2518FD CANFD data transfer, uncomment below lines
-// #undef  MAX_DATA_SIZE
-// #define MAX_DATA_SIZE 64
-
-#endif
-
-#ifdef CAN_2515
-#include "mcp2515_can.h"
-mcp2515_can CAN(SPI_CS_PIN); // Set CS pin
-#define MAX_DATA_SIZE 8
-#endif
 
 void setup()
 {
@@ -56,67 +13,55 @@ void setup()
     while (!Serial)
     {
     }
-
-#if MAX_DATA_SIZE > 8
-    /*
-     * To compatible with MCP2515 API,
-     * default mode is CAN_CLASSIC_MODE
-     * Now set to CANFD mode.
-     */
-    CAN.setMode(CAN_NORMAL_MODE);
-#endif
-
-    while (CAN_OK != CAN.begin(CAN_500KBPS))
-    { // init can bus : baudrate = 500k
-        Serial.println(F("CAN init fail, retry..."));
-        delay(100);
+    // start the CAN bus at 500 kbps
+    if (!CAN.begin(500E3))
+    {
+        Serial.println("Starting CAN failed!");
+        while (1)
+            ;
     }
-    Serial.println(F("CAN init ok!"));
 }
-
-uint32_t id;
-uint8_t type; // bit0: ext, bit1: rtr
-uint8_t len;
-byte cdata[MAX_DATA_SIZE] = {0};
-
 void loop()
 {
-    // check if data coming
-    if (CAN_MSGAVAIL != CAN.checkReceive())
+    int packetSize = CAN.parsePacket();
+
+    if (packetSize)
     {
-        return;
+        // received a packet
+        Serial.print("Received ");
+
+        if (CAN.packetExtended())
+        {
+            Serial.print("extended ");
+        }
+
+        if (CAN.packetRtr())
+        {
+            // Remote transmission request, packet contains no data
+            Serial.print("RTR ");
+        }
+
+        Serial.print("packet with id 0x");
+        Serial.print(CAN.packetId(), HEX);
+
+        if (CAN.packetRtr())
+        {
+            Serial.print(" and requested length ");
+            Serial.println(CAN.packetDlc());
+        }
+        else
+        {
+            Serial.print(" and length ");
+            Serial.println(packetSize);
+
+            // only print packet data for non-RTR packets
+            while (CAN.available())
+            {
+                Serial.print((char)CAN.read());
+            }
+            Serial.println();
+        }
+
+        Serial.println();
     }
-
-    char prbuf[32 + MAX_DATA_SIZE * 3];
-    int i, n;
-
-    unsigned long t = millis();
-    // read data, len: data length, buf: data buf
-    CAN.readMsgBuf(&len, cdata);
-
-    id = CAN.getCanId();
-    type = (CAN.isExtendedFrame() << 0) |
-           (CAN.isRemoteRequest() << 1);
-    /*
-     * MCP2515(or this driver) could not handle properly
-     * the data carried by remote frame
-     */
-
-    n = sprintf(prbuf, "%04lu.%03d ", t / 1000, int(t % 1000));
-    /* Displayed type:
-     *
-     * 0x00: standard data frame
-     * 0x02: extended data frame
-     * 0x30: standard remote frame
-     * 0x32: extended remote frame
-     */
-    static const byte type2[] = {0x00, 0x02, 0x30, 0x32};
-    n += sprintf(prbuf + n, "RX: [%08lX](%02X) ", (unsigned long)id, type2[type]);
-    // n += sprintf(prbuf, "RX: [%08lX](%02X) ", id, type);
-
-    for (i = 0; i < len; i++)
-    {
-        n += sprintf(prbuf + n, "%02X ", cdata[i]);
-    }
-    Serial.println(prbuf);
 }
