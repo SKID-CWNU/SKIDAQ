a3e8dc237101ff3baa7120e4de29c9f8c72f0f3f
Lim Chae Won <wonitch03@gmail.com>
2024-02-09 18:42:56 +0900

    Organize Display code from the Demo file

--- code/Display/Display.ino
+++ code/Display/Display.ino
@@ -1,41 +1,41 @@
+
 #include <Wire.h>
 #include <SPI.h>
-
-#define USE_UI // if you want to use the ui export from Squareline, please do not annotate this line.
-
-#if defined USE_UI
+#include <Arduino_GFX_Library.h>
+#include <ACAN_ESP32.h>
+#include <esp_chip_info.h>
+#include <esp_flash.h>
+#include <core_version.h>
 #include "lvgl.h"
 #include "ui.h"
 #include "lv_conf.h"
-#endif
-unsigned long watch_tick;
 
-#include <Arduino_GFX_Library.h>
+#define USE_UI // if you want to use the ui export from Squareline, please do not annotate this line.
+
 #define TFT_BL 2
 #define GFX_BL DF_GFX_BL // default backlight pin, you may replace DF_GFX_BL to actual backlight pin
 
 #define SERIAL_BAUD 115200
-#define I2C_DEV_ADDR 0x55
+#define rx 18
+#define tx 17
+#define drs 38
 
-extern lv_obj_t * ui_Spinbox1;
-extern lv_obj_t * ui_Spinbox2;
-extern lv_obj_t * ui_Spinbox3;
 int rpmVal = 0;
 int spVal = 0;
-int coolVal = 0;
+int drsVal = 0;
 int tempVal = 0;
 uint32_t i = 0;
-#define sda 17
-#define scl 18
+static const uint32_t DESIRED_BIT_RATE = 1000UL * 1000UL; // 1 Mb/s
 
-/******Please define a corresponding line based on your development board.************/
-#define Display_43
-// #define Display_50
-// #define Display_70
+extern lv_obj_t *ui_Spinbox1;
+extern lv_obj_t *ui_Spinbox2;
+extern lv_obj_t *ui_Spinbox3;
+extern lv_obj_t *ui_Label2;
+
 /*******************************************************************************
  * Screen Driver Configuration
  *******************************************************************************/
-#if defined(Display_43) // 4.3INCH 480x272
+
 Arduino_ESP32RGBPanel *bus = new Arduino_ESP32RGBPanel(
     GFX_NOT_DEFINED /* CS */, GFX_NOT_DEFINED /* SCK */, GFX_NOT_DEFINED /* SDA */,
     40 /* DE */, 41 /* VSYNC */, 39 /* HSYNC */, 42 /* PCLK */,
@@ -49,46 +49,11 @@ Arduino_RPi_DPI_RGBPanel *lcd = new Arduino_RPi_DPI_RGBPanel(
     272 /* height */, 0 /* vsync_polarity */, 8 /* vsync_front_porch */, 4 /* vsync_pulse_width */, 12 /* vsync_back_porch */,
     1 /* pclk_active_neg */, 7000000 /* prefer_speed */, true /* auto_flush */);
 
-#elif defined(Display_50) // 5.0INCH 800x480
-Arduino_ESP32RGBPanel *bus = new Arduino_ESP32RGBPanel(
-    GFX_NOT_DEFINED /* CS */, GFX_NOT_DEFINED /* SCK */, GFX_NOT_DEFINED /* SDA */,
-    40 /* DE */, 41 /* VSYNC */, 39 /* HSYNC */, 0 /* PCLK */,
-    45 /* R0 */, 48 /* R1 */, 47 /* R2 */, 21 /* R3 */, 14 /* R4 */,
-    5 /* G0 */, 6 /* G1 */, 7 /* G2 */, 15 /* G3 */, 16 /* G4 */, 4 /* G5 */,
-    8 /* B0 */, 3 /* B1 */, 46 /* B2 */, 9 /* B3 */, 1 /* B4 */
-);
-Arduino_RPi_DPI_RGBPanel *lcd = new Arduino_RPi_DPI_RGBPanel(
-    bus,
-    800 /* width */, 0 /* hsync_polarity */, 210 /* hsync_front_porch */, 4 /* hsync_pulse_width */, 43 /* hsync_back_porch */,
-    480 /* height */, 0 /* vsync_polarity */, 22 /* vsync_front_porch */, 4 /* vsync_pulse_width */, 12 /* vsync_back_porch */,
-    1 /* pclk_active_neg */, 16000000 /* prefer_speed */, true /* auto_flush */);
-
-#elif defined(Display_70) // 7.0INCH 800x480
-Arduino_ESP32RGBPanel *bus = new Arduino_ESP32RGBPanel(
-    GFX_NOT_DEFINED /* CS */, GFX_NOT_DEFINED /* SCK */, GFX_NOT_DEFINED /* SDA */,
-    41 /* DE */, 40 /* VSYNC */, 39 /* HSYNC */, 0 /* PCLK */,
-    14 /* R0 */, 21 /* R1 */, 47 /* R2 */, 48 /* R3 */, 45 /* R4 */,
-    9 /* G0 */, 46 /* G1 */, 3 /* G2 */, 8 /* G3 */, 16 /* G4 */, 1 /* G5 */,
-    15 /* B0 */, 7 /* B1 */, 6 /* B2 */, 5 /* B3 */, 4 /* B4 */
-);
-Arduino_RPi_DPI_RGBPanel *lcd = new Arduino_RPi_DPI_RGBPanel(
-    bus,
-    800 /* width */, 0 /* hsync_polarity */, 210 /* hsync_front_porch */, 1 /* hsync_pulse_width */, 46 /* hsync_back_porch */,
-    480 /* height */, 0 /* vsync_polarity */, 22 /* vsync_front_porch */, 1 /* vsync_pulse_width */, 23 /* vsync_back_porch */,
-    0 /* pclk_active_neg */, 16000000 /* prefer_speed */, true /* auto_flush */);
-#endif
-
 /*******************************************************************************
  * Screen Driver Configuration  end
  *******************************************************************************/
 
-/*******************************************************************************
-   Please config the touch panel in touch.h
- ******************************************************************************/
 #include "touch.h"
-
-#ifdef USE_UI
-/* Change to your screen resolution */
 static uint32_t screenWidth;
 static uint32_t screenHeight;
 static lv_disp_draw_buf_t draw_buf;
@@ -111,95 +76,83 @@ void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color
   lv_disp_flush_ready(disp);
 }
 
-void my_touchpad_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data)
-{
-  if (touch_has_signal())
-  {
-    if (touch_touched())
-    {
-      data->state = LV_INDEV_STATE_PR;
-
-      /*Set the coordinates*/
-      data->point.x = touch_last_x;
-      data->point.y = touch_last_y;
-      Serial.print("Data x :");
-      Serial.println(touch_last_x);
-
-      Serial.print("Data y :");
-      Serial.println(touch_last_y);
-    }
-    else if (touch_released())
-    {
-      data->state = LV_INDEV_STATE_REL;
-    }
-  }
-  else
-  {
-    data->state = LV_INDEV_STATE_REL;
-  }
-  delay(15);
-}
-#endif
-
-void onRequest()
-{
-  Wire.print(i++);
-  Wire.print(" Packets.");
-  Serial.println("onRequest");
-}
-void onReceive(int len)
-{
-  Serial.printf("onReceive[%d]: ", len);
-  while (Wire.available())
-  {
-    spVal = Wire.read();
-    lv_spinbox_set_value(ui_Spinbox2, spVal);
-    Serial.write(Wire.read());
-  }
-  Serial.println();
-}
+void my_touchpad_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data) {}
 
 void setup()
 {
   Serial.begin(SERIAL_BAUD);
-  Serial.println("LVGL Widgets Demo");
   Serial.setDebugOutput(true);
-  Wire.onReceive(onReceive);
-  Wire.onRequest(onRequest);
-  Wire.setPins(sda, scl);
-  Wire.begin((uint8_t)I2C_DEV_ADDR);
+  esp_chip_info_t chip_info;
+  esp_chip_info(&chip_info);
+  Serial.print("ESP32 Arduino Release: ");
+  Serial.println(ARDUINO_ESP32_RELEASE);
+  Serial.print("ESP32 Chip Revision: ");
+  Serial.println(chip_info.revision);
+  Serial.print("ESP32 SDK: ");
+  Serial.println(ESP.getSdkVersion());
+  Serial.print("ESP32 Flash: ");
+  uint32_t size_flash_chip;
+  esp_flash_get_size(NULL, &size_flash_chip);
+  Serial.print(size_flash_chip / (1024 * 1024));
+  Serial.print(" MB ");
+  Serial.println(((chip_info.features & CHIP_FEATURE_EMB_FLASH) != 0) ? "(embeded)" : "(external)");
+  Serial.print("APB CLOCK: ");
+  Serial.print(APB_CLK_FREQ);
+  Serial.println(" Hz");
+  //--- Configure ESP32 CAN
+  Serial.println("Configure ESP32 CAN");
+  ACAN_ESP32_Settings settings(DESIRED_BIT_RATE);
+  settings.mRequestedCANMode = ACAN_ESP32_Settings::LoopBackMode;
+  settings.mRxPin = GPIO_NUM_17; // Optional, default Tx pin is GPIO_NUM_4
+  settings.mTxPin = GPIO_NUM_18; // Optional, default Rx pin is GPIO_NUM_5
+  const uint32_t errorCode = ACAN_ESP32::can.begin(settings);
+  if (errorCode == 0)
+  {
+    Serial.print("Bit Rate prescaler: ");
+    Serial.println(settings.mBitRatePrescaler);
+    Serial.print("Time Segment 1:     ");
+    Serial.println(settings.mTimeSegment1);
+    Serial.print("Time Segment 2:     ");
+    Serial.println(settings.mTimeSegment2);
+    Serial.print("RJW:                ");
+    Serial.println(settings.mRJW);
+    Serial.print("Triple Sampling:    ");
+    Serial.println(settings.mTripleSampling ? "yes" : "no");
+    Serial.print("Actual bit rate:    ");
+    Serial.print(settings.actualBitRate());
+    Serial.println(" bit/s");
+    Serial.print("Exact bit rate ?    ");
+    Serial.println(settings.exactBitRate() ? "yes" : "no");
+    Serial.print("Distance            ");
+    Serial.print(settings.ppmFromDesiredBitRate());
+    Serial.println(" ppm");
+    Serial.print("Sample point:       ");
+    Serial.print(settings.samplePointFromBitStart());
+    Serial.println("%");
+    Serial.println("Configuration OK!");
+  }
+  else
+  {
+    Serial.print("Configuration error 0x");
+    Serial.println(errorCode, HEX);
+  }
 
-#if defined(Display_50) || defined(Display_70)
-  // IO Port Pins
-  pinMode(38, OUTPUT);
-  digitalWrite(38, LOW);
-  pinMode(17, OUTPUT);
-  digitalWrite(17, LOW);
-  pinMode(18, OUTPUT);
-  digitalWrite(18, LOW);
-  pinMode(42, OUTPUT);
-  digitalWrite(42, LOW);
-#elif defined(Display_43)
   pinMode(20, OUTPUT);
   digitalWrite(20, LOW);
   pinMode(19, OUTPUT);
   digitalWrite(19, LOW);
   pinMode(35, OUTPUT);
   digitalWrite(35, LOW);
-  pinMode(38, OUTPUT);
-  digitalWrite(38, LOW);
+  pinMode(drs, INPUT_PULLUP);
+  // digitalWrite(38, LOW);
   pinMode(0, OUTPUT); // TOUCH-CS
-#endif
 
   // Init Display
   lcd->begin();
   lcd->fillScreen(BLACK);
   lcd->setTextSize(2);
   delay(200);
-
-#ifdef USE_UI
   lv_init();
-
   delay(100);
   touch_init();
 
@@ -223,36 +176,65 @@ void setup()
   indev_drv.type = LV_INDEV_TYPE_POINTER;
   indev_drv.read_cb = my_touchpad_read;
   lv_indev_drv_register(&indev_drv);
-#endif
 
 #ifdef TFT_BL
   pinMode(TFT_BL, OUTPUT);
   digitalWrite(TFT_BL, HIGH);
 #endif
-
-#ifdef USE_UI
   ui_init(); // ui from Squareline or GUI Guider
-#else
-  lcd->fillScreen(RED);
-  delay(800);
-  lcd->fillScreen(BLUE);
-  delay(800);
-  lcd->fillScreen(YELLOW);
-  delay(800);
-  lcd->fillScreen(GREEN);
-  delay(800);
-#endif
   Serial.println("Setup done");
+  delay(3000);
 }
+
+static uint32_t gBlinkLedDate = 0;
+static uint32_t gReceivedFrameCount = 0;
+static uint32_t gSentFrameCount = 0;
 
 void loop()
 {
-
-  while (1)
+  drsVal = digitalRead(drs);
+  CANMessage frame;
+  if (gBlinkLedDate < millis())
   {
-#ifdef USE_UI
-    lv_timer_handler();
-    delay(5);
-#endif
+    gBlinkLedDate += 500;
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+    Serial.print("Sent: ");
+    Serial.print(gSentFrameCount);
+    Serial.print(" ");
+    Serial.print("Receive: ");
+    Serial.print(gReceivedFrameCount);
+    Serial.print(" ");
+    Serial.print(" STATUS 0x");
+    Serial.print(TWAI_STATUS_REG, HEX);
+    Serial.print(" RXERR ");
+    Serial.print(TWAI_RX_ERR_CNT_REG);
+    Serial.print(" TXERR ");
+    Serial.println(TWAI_TX_ERR_CNT_REG);
+    const bool ok = ACAN_ESP32::can.tryToSend(frame);
+    if (ok)
+    {
+      gSentFrameCount += 1;
+    }
   }
+  while (ACAN_ESP32::can.receive(frame))
+  {
+    gReceivedFrameCount += 1;
+  }
+
+  lv_timer_handler();
+  lv_spinbox_set_value(ui_Spinbox2, spVal);
+
+  if (drsVal == 1)
+  {
+    lv_label_set_text(ui_Label2, "ON");
+  }
+  else if (drsVal == 0)
+  {
+    lv_label_set_text(ui_Label2, "OFF");
+  }
+  else
+  {
+    lv_label_set_text(ui_Label2, "N/A");
+  }
+  delay(100);
 }
