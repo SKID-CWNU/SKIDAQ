2933dccfa2919d871860bada4dc72c3e65179f18
Lim Chae Won <wonitch03@gmail.com>
2024-04-12 14:47:19 +0900

    Sync

--- code/Testing_Crate/GPSTest/GPSTest.ino
+++ code/Testing_Crate/GPSTest/GPSTest.ino
@@ -1,33 +1,10 @@
-#include <SoftwareSerial.h>
+const int fan = 3;
 
-// Choose two Arduino pins to use for software serial
-int RXPin = 5;
-int TXPin = 4;
-
-// Default baud of NEO-6M is 9600
-int GPSBaud = 9600;
-
-// Create a software serial port called "gpsSerial"
-SoftwareSerial gpsSerial(RXPin, TXPin);
-
-void setup()
-{
-    // Start the Arduino hardware serial port at 9600 baud
-    Serial.begin(9600);
-
-    // Start the software serial port at the GPS's default baud
-    gpsSerial.begin(GPSBaud);
+void setup() {
+  pinMode(fan, OUTPUT);
 }
 
-void loop()
-{
-    // Displays information when new sentence is available.
-    while (gpsSerial.available() > 0)
-    {
-        Serial.write(gpsSerial.read());
-    }
-    while (Serial.available() > 0)
-    {
-        gpsSerial.write(Serial.read());
-    }
+void loop() {
+  int fanSpeed = 255;
+  analogWrite(fan, fanSpeed);
 }
